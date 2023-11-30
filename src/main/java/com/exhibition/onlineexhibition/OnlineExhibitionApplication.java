7dd88a63018a7ced099420c5bda86deee8ac1e24
jeevaathecoder <jeevaathecoder@gmail.com>
2023-11-30 09:52:26 +0530

    initial commit

---
+++ src/main/java/com/exhibition/onlineexhibition/OnlineExhibitionApplication.java
@@ -0,0 +1,14 @@
+package com.exhibition.onlineexhibition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineExhibitionApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(OnlineExhibitionApplication.class, args);
+    }
+
+}
